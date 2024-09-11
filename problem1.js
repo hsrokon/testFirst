3a4bca61c86638aa030d2c818b6d846184e9e959
MD. HUSSAIN SHAHRIAR ROKON <hsrokonofficial@gmail.com>
2024-09-11 19:35:24 +0600

    problem2 Good/Bad name

--- problem1.js
+++ problem1.js
@@ -1,5 +1,7 @@
 
+// problem1
 
+/*
 function calculateMoney(ticketSale) {
     if (ticketSale<0) {
         return 'Invalid Number'
@@ -15,4 +17,33 @@ console.log(calculateMoney(10));
 console.log(calculateMoney(1055));
 console.log(calculateMoney(93));
 console.log(calculateMoney(-130));
-console.log(calculateMoney(3));
+console.log(calculateMoney(3)); 
+*/
+
+// problem2
+
+function checkName(name){
+    if (typeof name !== "string") {
+        return 'Invalid Name'
+    }
+    let lastLetter =name[name.length-1].toLowerCase();
+
+    const goodLetters =['a', 'y', 'i', 'e', 'o', 'u', 'w'];
+        
+    let var1 = goodLetters.includes(lastLetter)
+
+       /* if (var1) {
+            return "good name"
+        } else {
+            return "bad name"
+        }
+        */
+        const goodName = var1 ? "Good name" : "Bad name";
+        return goodName
+}
+
+console.log (checkName('hhuh'))
+console.log (checkName('AFEE'))
+console.log (checkName('Jhankar'))
+console.log (checkName(199))
+console.log (checkName(["Rashed"]))
