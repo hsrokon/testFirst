8f69d621a013caf17d036be86cc3677f9c2af553
hsrokon <hsrokonofficial@gmail.com>
2025-02-19 17:59:56 +0600

    Map and shortcut uses of Map

---
+++ Problems/map.js
@@ -0,0 +1,17 @@
+const numbers = [3, 4, 8, 6];
+
+const doubledNumbers = [];
+for (const num of numbers) {
+    const doubled = num*2;
+    doubledNumbers.push(doubled);
+}
+console.log(doubledNumbers);
+
+//shortcut
+const doubledNum = numbers.map(n => n*2);//map loops an array puts in function & returns array
+console.log(doubledNum);
+
+const classMates = ['Max', 'John', 'Andrew', "Gabriel"];
+
+const length = classMates.map(c => c.length); console.log(length);
+const fLetter = classMates.map(f => f[0]); console.log(fLetter);
