b7b08b326900704c32479cbcba9bce9bd33ffac5
hsrokon <hsrokonofficial@gmail.com>
2025-02-27 23:16:50 +0600

    Clarified some comments

--- Problems/passby.js
+++ Problems/passby.js
@@ -6,7 +6,7 @@ function passBy(a, b) {
     const multiply = a * b;
     return multiply;
 }
-//Primitive types are passed by value
+//Primitive types are passed by value, when passed into fun it creates a copy of it
 console.log(num1);
 const var1 = passBy(num1, num2);//after calling the function, variable does not change
 console.log(num1);
@@ -20,7 +20,8 @@ function passBy1(a, b) {
     object1.name = "Abrar";
     object2.name = "Hasan";
 }
-//Non-primitive types are passed by reference
+//Non-primitive types are passed by reference, when passed into a fun it indicates the same ref without creating a copy
+
 console.log(object1, object2);//{ name: 'Zahir', is: 'student' } { name: 'Raihan', is: 'teacher' }
 const var2 = passBy1(object1, object2);//after calling the function, variable changes because of change in reference
 console.log(object1, object2);//{ name: 'Abrar', is: 'student' } { name: 'Hasan', is: 'teacher' }
